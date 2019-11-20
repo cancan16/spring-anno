ffb906f5148de5bbff89b95eb5d7df75d1fa9244
liucancan <916215796@qq.com>
2019-11-20 14:55:09 +0800

    no message

--- src/main/java/com/enjoy/cap1/MainTest2.java
+++ src/main/java/com/enjoy/cap1/MainTest2.java
@@ -1,28 +1,26 @@
 package com.enjoy.cap1;
 
+import com.enjoy.cap1.config.MainConfig;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.enjoy.cap1.config.MainConfig;
+public class MainTest2 {
+    public static void main(String args[]) {
+        //把beans.xml的类加载到容器
+        //ApplicationContext app = new ClassPathXmlApplicationContext("beans.xml");
 
-public class MainTest2 { 
-	public static void main(String args[]){
-		//把beans.xml的类加载到容器
-		//ApplicationContext app = new ClassPathXmlApplicationContext("beans.xml");
-		
-		ApplicationContext app = new AnnotationConfigApplicationContext(MainConfig.class);
-		
-		//从容器中获取bean
-		//Person person = (Person) app.getBean("person01");
-		
-		//System.out.println(person);
-		
-		String[] namesForBean = app.getBeanNamesForType(Person.class);
-		for(String name:namesForBean){
-			System.out.println(name);
-		}
-		
-		
-	}
+        ApplicationContext app = new AnnotationConfigApplicationContext(MainConfig.class);
+
+        //从容器中获取bean
+        Person person = (Person) app.getBean("abcPerson");
+        System.out.println(person.toString());
+        //System.out.println(person);
+
+        String[] namesForBean = app.getBeanNamesForType(Person.class);
+        for (String name : namesForBean) {
+            System.out.println(name);
+        }
+
+
+    }
 }
