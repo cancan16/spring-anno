6d2dc78b545c55df8bda326f0ea3b5390c097e6d
liucancan <916215796@qq.com>
2019-12-04 17:01:48 +0800

    no message

--- src/test/java/Cap5Test.java
+++ src/test/java/Cap5Test.java
@@ -1,15 +1,14 @@
+import com.enjoy.cap5.config.Cap5MainConfig;
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import com.enjoy.cap5.config.Cap5MainConfig;
-
 public class Cap5Test {
-	@Test
-	public void test01(){
-		AnnotationConfigApplicationContext app = new AnnotationConfigApplicationContext(Cap5MainConfig.class);
-		
-		System.out.println("IOC容器创建完成........");
-		
-		
-	}
+    @Test
+    public void test01() {
+        AnnotationConfigApplicationContext app = new AnnotationConfigApplicationContext(Cap5MainConfig.class);
+
+        System.out.println("IOC容器创建完成........");
+
+
+    }
 }
