7788c3636f37e51bc6e52edeaa86f8623da3e193
liucancan <916215796@qq.com>
2019-12-04 18:14:49 +0800

    no message

--- src/main/java/com/enjoy/cap6/config/JamesFactoryBean.java
+++ src/main/java/com/enjoy/cap6/config/JamesFactoryBean.java
@@ -1,25 +1,24 @@
 package com.enjoy.cap6.config;
 
+import com.enjoy.cap6.bean.Monkey;
 import org.springframework.beans.factory.FactoryBean;
 
-import com.enjoy.cap6.bean.Monkey;
+public class JamesFactoryBean implements FactoryBean<Monkey> {
 
-public class JamesFactoryBean implements FactoryBean<Monkey>{
+    @Override
+    public Monkey getObject() throws Exception {
+        // TODO Auto-generated method stub
+        return new Monkey();
+    }
 
-	@Override
-	public Monkey getObject() throws Exception {
-		// TODO Auto-generated method stub
-		return new Monkey();
-	}
+    @Override
+    public Class<?> getObjectType() {
+        // TODO Auto-generated method stub
+        return Monkey.class;
+    }
 
-	@Override
-	public Class<?> getObjectType() {
-		// TODO Auto-generated method stub
-		return Monkey.class;
-	}
-	
-	@Override
-	public boolean isSingleton() {
-		return true;
-	}
+    @Override
+    public boolean isSingleton() {
+        return true;
+    }
 }
