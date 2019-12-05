1fcf18c8fc75047178ecfe75712a7548c113f6af
liucancan <916215796@qq.com>
2019-12-05 18:16:41 +0800

    no message

---
+++ src/main/java/com/enjoy/cap7/config/Cap7MainConfigOfLifeCycle.java
@@ -0,0 +1,29 @@
+package com.enjoy.cap7.config;
+
+import com.enjoy.cap1.Person;
+import com.enjoy.cap7.bean.Bike;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("com.enjoy.cap7.bean")
+@Configuration
+public class Cap7MainConfigOfLifeCycle {
+    @Bean("person")
+    public Person person() {
+        System.out.println("给容器中添加person.......");
+        return new Person("person", 20);
+    }
+
+    /**
+     * @Date 17:22 2019/12/5
+     * @Description 指定初始化方法和销毁方法
+     * @Return com.enjoy.cap7.bean.Bike
+     * @Param []
+     */
+    @Bean(initMethod = "init", destroyMethod = "destory")
+    public Bike bike() {
+        return new Bike();
+    }
+
+}
