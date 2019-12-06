ced4a8069936936f47240719f0e036b08389850e
liucancan <916215796@qq.com>
2019-12-06 15:23:14 +0800

    no message

--- src/main/java/com/enjoy/cap7/bean/Train.java
+++ src/main/java/com/enjoy/cap7/bean/Train.java
@@ -11,14 +11,19 @@ public class Train implements InitializingBean, DisposableBean {
         System.out.println("Train......constructor............");
     }
 
-    //当我们bean销毁时,调用此方法
+    // 当我们bean销毁时,调用此方法
     @Override
     public void destroy() throws Exception {
         System.out.println("Train......destory......");
         //logger.error
     }
 
-    //当我们的bean属性赋值和初始化完成时调用
+    /**
+     * @Date 15:17 2019/12/6
+     * @Description 所有属性设置好之后, 会调这个方法, 相当于初始化方法
+     * @Return void
+     * @Param []
+     */
     @Override
     public void afterPropertiesSet() throws Exception {
         System.out.println("Train.......afterPropertiesSet()...");
--- src/main/java/com/enjoy/cap7/config/Cap7MainConfigOfLifeCycle.java
+++ src/main/java/com/enjoy/cap7/config/Cap7MainConfigOfLifeCycle.java
@@ -1,6 +1,5 @@
 package com.enjoy.cap7.config;
 
-import com.enjoy.cap1.Person;
 import com.enjoy.cap7.bean.Bike;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,11 +8,11 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan("com.enjoy.cap7.bean")
 @Configuration
 public class Cap7MainConfigOfLifeCycle {
-    @Bean("person")
-    public Person person() {
-        System.out.println("给容器中添加person.......");
-        return new Person("person", 20);
-    }
+//    @Bean("person")
+//    public Person person() {
+//        System.out.println("给容器中添加person.......");
+//        return new Person("person", 20);
+//    }
 
     /**
      * @Date 17:22 2019/12/5
