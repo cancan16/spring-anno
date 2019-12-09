db3ad62887cf575a8107aff079c25cb84bd2aff0
liucancan <916215796@qq.com>
2019-12-09 11:30:44 +0800

    no message

--- src/test/java/Cap7Test.java
+++ src/test/java/Cap7Test.java
@@ -1,9 +1,11 @@
 import com.enjoy.cap7.config.Cap7MainConfigOfLifeCycle;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Cap7Test {
     @Test
+    @Autowired
     public void test01() {
         AnnotationConfigApplicationContext app = new AnnotationConfigApplicationContext(Cap7MainConfigOfLifeCycle.class);
 
