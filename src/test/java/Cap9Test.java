5a49d93c3ab91e7630552b8270057e207ea1265a
liucancan <916215796@qq.com>
2019-12-10 15:48:16 +0800

    no message

--- src/test/java/Cap9Test.java
+++ src/test/java/Cap9Test.java
@@ -1,6 +1,4 @@
 import com.enjoy.cap9.config.Cap9MainConfig;
-import com.enjoy.cap9.dao.TestDao;
-import com.enjoy.cap9.service.TestService;
 import org.junit.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -8,11 +6,13 @@ public class Cap9Test {
     @Test
     public void test01() {
         AnnotationConfigApplicationContext app = new AnnotationConfigApplicationContext(Cap9MainConfig.class);
-        TestService testService = app.getBean(TestService.class);
-        testService.println();
-        //直接从容器中获取TestDao, 和使用Autowired注解来取比较
-//        TestDao testDao = app.getBean(TestDao.class);
-//        System.out.println(testDao);
+//        TestService testService = app.getBean(TestService.class);
+//        testService.println();
+
+//        Moon moon = app.getBean(Moon.class);
+//        System.out.println(moon);
+//        Sun sun = app.getBean(Sun.class);
+//        System.out.println(sun.getMoon());
         app.close();
     }
 }
