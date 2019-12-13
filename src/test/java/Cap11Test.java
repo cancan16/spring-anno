405b939c6d308e564593bd7e2f242030259c4fc0
liucancan <916215796@qq.com>
2019-12-13 16:13:28 +0800

    no message

---
+++ src/test/java/Cap11Test.java
@@ -0,0 +1,18 @@
+import com.enjoy.cap11.config.Cap11MainConfig;
+import com.enjoy.cap11.service.OrderService;
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Cap11Test {
+    @Test
+    public void test01() {
+        AnnotationConfigApplicationContext app = new AnnotationConfigApplicationContext(Cap11MainConfig.class);
+
+        OrderService bean = app.getBean(OrderService.class);
+        bean.addOrder();
+
+        app.close();
+
+
+    }
+}
